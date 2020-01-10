5fae3aecf57150562b64e0b39e98b9ac5e1b49f5
z-hoang-vuphuong <z.hoang.vuphuong@gree.net>
2020-01-10 16:09:30 +0700

    Add temp voice & wake-up SFX, update deal BGM

--- assets/mixer_data.js
+++ assets/mixer_data.js
@@ -2,17 +2,23 @@ let data = {
   "3-b-intro": {
      "name":"3-b-intro",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.55,
      "cue_begin":"đầu",
      "cue_end":"hết nhạc",
      "fade_out_duration":2,
      "duck_fade_duration":-1,
      "duck_volume": 0.2
   },
+  "3b-s-introduce": {
+     "name":"3b-s-introduce",
+     "is_bgm":false,
+     "volume":1,
+     "cue_begin":"lúc DẪN TRUYỆN (sẽ thu âm đè vào `3-b-intro`)"
+  },
   "4-b-romance": {
      "name":"4-b-romance",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"lúc Nở kéo Chí vào bụi chuối",
      "cue_end":"&lt;bgm&gt; tiếp theo (TỰ ĐỘNG)",
      "fade_out_duration":-1,
@@ -22,17 +28,23 @@ let data = {
   "6-b-tree-falls-AND-romance": {
      "name":"6-b-tree-falls-AND-romance",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"lúc bụi chuối đổ",
      "cue_end":"&lt;bgm&gt; tiếp theo (TỰ ĐỘNG)",
      "fade_out_duration":1,
      "duck_fade_duration":-1,
      "duck_volume": 0.2
   },
+  "6b-s-drop-banana-tree": {
+     "name":"6b-s-drop-banana-tree",
+     "is_bgm":false,
+     "volume":1,
+     "cue_begin":"sau tiếng bụi chuối đổ (sẽ thu âm đè vào BGM)"
+  },
   "7-b-enlighten": {
      "name":"7-b-enlighten",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"cuối câu [phải sờ-tút-tát thôi]",
      "cue_end":"khi chạy SFX &lt;post-scene-setup&gt;",
      "fade_out_duration":6,
@@ -42,13 +54,13 @@ let data = {
   "7b-s-post-scene-setup": {
      "name":"7b-s-post-scene-setup",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"lúc setup xong ghế"
   },
   "8-b-shark-tank": {
      "name":"8-b-shark-tank",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.6,
      "cue_begin":"lúc bắt đầu câu [Shark Tank]",
      "cue_end":"khi giới thiệu xong cả 3 shark",
      "fade_out_duration":3,
@@ -58,25 +70,25 @@ let data = {
   "9-s-cheer-1": {
      "name":"9-s-cheer-1",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":0.5,
      "cue_begin":"lúc giới thiệu Shark Dậu"
   },
   "9-s-cheer-2": {
      "name":"9-s-cheer-2",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":0.5,
      "cue_begin":"lúc giới thiệu Shark Hạc"
   },
   "9-s-cheer-3": {
      "name":"9-s-cheer-3",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":0.5,
      "cue_begin":"lúc giới thiệu Shark Kiều"
   },
   "10b-b-entrance": {
      "name":"10b-b-entrance",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"lúc bắt đầu catwalk",
      "cue_end":"hết nhạc",
      "fade_out_duration":3,
@@ -86,7 +98,7 @@ let data = {
   "10d-b-shark-tank": {
      "name":"10d-b-shark-tank",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"cuối câu [đồ uống có cồn]",
      "cue_end":"khi 2 PG ra khỏi sân khấu",
      "fade_out_duration":3,
@@ -96,7 +108,7 @@ let data = {
   "12-b-deal": {
      "name":"12-b-deal",
      "is_bgm":true,
-     "volume":0.7,
+     "volume":0.4,
      "cue_begin":"sau khi các shark xem rượu xong",
      "cue_end":"cuối câu [cho đừng đập nhau]",
      "fade_out_duration":1.5,
@@ -106,49 +118,49 @@ let data = {
   "12-s-wow": {
      "name":"12-s-wow",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [300 cây vàng cho 1% cổ phần]"
   },
   "13a-s-boom": {
      "name":"13a-s-boom",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [đừng ngáo giá]"
   },
   "13b-s-heavenly-choir": {
      "name":"13b-s-heavenly-choir",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [nằm ở tương lai]"
   },
   "13c-s-ding": {
      "name":"13c-s-ding",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [phải đi nhiều tiền]"
   },
   "14a-s-shock": {
      "name":"14a-s-shock",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [bán thêm con chó là đủ tiền cho em nó ấy mà]"
   },
   "14b-s-shock": {
      "name":"14b-s-shock",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [giỏi nhất là bốc phét đấy anh Hạc ạ]"
   },
   "14c-s-ohhhh": {
      "name":"14c-s-ohhhh",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [Chỉ mỗi biết điều là không biết thôi]"
   },
   "15-b-nervous": {
      "name":"15-b-nervous",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"cuối câu [anh làm công việc gì nhỉ]",
      "cue_end":"hết nhạc",
      "fade_out_duration":3,
@@ -158,31 +170,31 @@ let data = {
   "15a-s-nani": {
      "name":"15a-s-nani",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [vị trí bảo vệ tầng 2]"
   },
   // "15b-s-nani": {
   //    "name":"15b-s-nani",
   //    "is_bgm":false,
-  //    "volume":0.7,
+  //    "vo1,
   //    "cue_begin":"cuối câu [vị trí bảo vệ còn trống]"
   // },
   // "15c-s-boom": {
   //    "name":"15c-s-boom",
   //    "is_bgm":false,
-  //    "volume":0.7,
+  //    "vo1,
   //    "cue_begin":"cuối câu [vị trí bảo vệ tầng 2]"
   // },
   // "16a-s-silence": {
   //    "name":"16a-s-silence",
   //    "is_bgm":false,
-  //    "volume":0.7,
+  //    "vo1,
   //    "cue_begin":"cuối câu [Công thức nấu rượu gia truyền và em]"
   // },
   "15d-b-swan": {
      "name":"15d-b-swan",
      "is_bgm":true,
-     "volume":0.8,
+     "volume":0.6,
      "cue_begin":"câu [Cuộc sống giống như 1 bản tango]",
      "cue_end":"&lt;bgm&gt; tiếp theo (TỰ ĐỘNG)",
      "fade_out_duration":-1,
@@ -202,43 +214,43 @@ let data = {
   // "17b-s-whistle": {
   //    "name":"17b-s-whistle",
   //    "is_bgm":false,
-  //    "volume":0.7,
+  //    "vo1,
   //    "cue_begin":"cuối câu [có đầu tư không mà hỏi lắm thế]"
   // },
    "17c-s-shock": {
      "name":"17c-s-shock",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [lại bắt đầu hội chứng thích ăn đòn rồi đấy]"
   },
   "18a-s-nani": {
      "name":"18a-s-nani",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [start-up bằng vốn tự có thôi ạ]"
   },
   "18b-s-awwww": {
      "name":"18b-s-awwww",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [học nhiều thì ngu]"
   },
   "18c-s-ohhhh": {
      "name":"18c-s-ohhhh",
      "is_bgm":false,
-     "volume":0.7,
+     "volume":1,
      "cue_begin":"cuối câu [thì thua con cầy]"
   },
   // "18d-s-whistle": {
   //    "name":"18d-s-whistle",
   //    "is_bgm":false,
-  //    "volume":0.7,
+  //    "vo1,
   //    "cue_begin":"cuối câu [vựa trái cây đây này]"
   // },
   "19a-b-sad": {
      "name":"19a-b-sad",
      "is_bgm":true,
-     "volume":0.8,
+     "volume":0.7,
      "cue_begin":"cuối câu [Tôi quyết định không đầu tư]",
      "cue_end":"hết nhạc",
      "fade_out_duration":3,
@@ -248,7 +260,7 @@ let data = {
   "19e-b-fight": {
      "name":"19e-b-fight",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"cuối câu [kẻ thì bán con, đứa thì bán mình]",
      "cue_end":"hết nhạc",
      "fade_out_duration":3,
@@ -270,7 +282,7 @@ let data = {
   "22a-b-explain": {
      "name":"22a-b-explain",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.85,
      "cue_begin":"lúc Nở ngã, --->DUCK [chưa thể bắt đầu ... Đâu phải ai]",
      "cue_end":"cuối câu [hay nói đạo lý]",
      "fade_out_duration":2,
@@ -280,7 +292,7 @@ let data = {
   "22b-b-shame": {
      "name":"22b-b-shame",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"cuối câu [Anh Chí, anh cứ cố gắng]",
      "cue_end":"hết nhạc",
      "fade_out_duration":3,
@@ -290,12 +302,18 @@ let data = {
   "23-b-think": {
      "name":"23-b-think",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"lúc chị Dậu bắt đầu giảng giải",
      "cue_end":"cuối câu [không đầu hàng trước nghịch cảnh]",
      "fade_out_duration":5,
      "duck_fade_duration":-1,
      "duck_volume": 0.2
+  },
+  "24a-s-wake-up": {
+     "name":"24a-s-wake-up",
+     "is_bgm":false,
+     "volume":1,
+     "cue_begin":"lúc Nở giơ tay bật dậy"
   },
   "24-b-wake-up": {
      "name":"24-b-wake-up",
@@ -310,7 +328,7 @@ let data = {
   "25-b-dance": {
      "name":"25-b-dance",
      "is_bgm":true,
-     "volume":1,
+     "volume":0.7,
      "cue_begin":"cuối câu [còn bây giờ thì quan trọng nhất là]",
      "cue_end":"hết nhạc",
      "fade_out_duration":3,
